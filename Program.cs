bfa2ab49ffe988c65f19399f8a23d970765c21d5
Sander <sanderveldt98@gmail.com>
2026-06-22 22:19:06 +0200

    rectangle finshed

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace First_project
+namespace Shape_calc
 {
     class Program
     {
@@ -23,31 +23,60 @@ namespace First_project
                 {
                     switch (shape_choice)
                     {
-                        case "1":
+                        case 1:
                             Console.WriteLine("Enter side length in cm:");
+
                             string input_square_side = Console.ReadLine();
                             if (int.TryParse(input_square_side, out int square_side))
                             {
-                                int area = square_side * square_side;
-                                Console.WriteLine("The sides are " + side + "cm.\n" +
-                                                  "The area of the square is " + area + "cm2.");
-                                break;
+                                int square_area = square_side * square_side;
+
+                                Console.WriteLine("The sides are " + square_side + "cm.");
+                                Console.WriteLine("The area of the square is " + square_area + "cm2.\n");
                             }
                             else
                             {
-                                Console.WriteLine("Invalid input, please choose a number.")
+                                Console.WriteLine("Invalid input, please choose a number.");
                             }
+                            break;
 
-                        case "2":
+                        case 2:
                             Console.WriteLine("Enter short side's length in cm:");
-                            string input = Console.ReadLine();
+
+                            string rectangle_input1 = Console.ReadLine();
+                            if (int.TryParse(rectangle_input1, out int rectangle_side1))
+                            {
+                                Console.WriteLine("Enter long side's length in cm:");
+                                string rectangle_input2 = Console.ReadLine();
+                                if (int.TryParse(rectangle_input2, out int rectangle_side2))
+                                {
+                                    int rectangle_area = rectangle_side1 * rectangle_side2;
+
+                                    Console.WriteLine("The short side is " + rectangle_input1 + "cm.");
+                                    Console.WriteLine("The long side is " + rectangle_input2 + "cm.");
+                                    Console.WriteLine("The area of the rectangle is " + (rectangle_area) + "cm2.\n");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Invalid input, please choose a number.");
+                                }
+                            }
+                            else
+                            {
+                                Console.WriteLine("Invalid input, please choose a number.");
+                            }
+                            break;
 
 
                         default:
                             break;
 
-
+                    
                     }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, please choose a number");
                 }
             }
         }
